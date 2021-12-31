d5c37d523ec40389c7e59f53f20688b4f6399692
hwan-cs <hwan333@konkuk.ac.kr>
2021-12-31 17:01:36 +0900

    Initial Commit

---
+++ Hatcha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hatcha
+//
+//  Created by Jung Hwan Park on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

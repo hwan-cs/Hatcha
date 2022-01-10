6bc3da9107140d774f48397803aee7031f9089a5
hwan-cs <hwann1005@gmail.com>
2022-01-11 02:55:10 +0900

    Audio normalization implementation!!!!!!

--- Hatcha/Controller/AlarmViewController.swift
+++ Hatcha/Controller/AlarmViewController.swift
@@ -8,10 +8,7 @@
 import UIKit
 import Speech
 import AudioKit
-import AudioUnit
 import CoreAudioKit
-import CAudioKit
-import AVFAudio
 import Accelerate
 
 class AlarmViewController: UIViewController, SFSpeechRecognizerDelegate
@@ -37,97 +34,53 @@ class AlarmViewController: UIViewController, SFSpeechRecognizerDelegate
         self.navigationController?.navigationBar.isTranslucent = true
         self.navigationController?.view.backgroundColor = .clear
         
+        print("????")
         speechRecognizer.delegate = self
         requestPermission()
         let audioURL = Bundle.main.url(forResource: "audiotest", withExtension: "mp3")
         
-//        equalizer = AVAudioUnitEQ(numberOfBands: 0)
-//        audioEngine.attach(audioPlayerNode)
-//        audioEngine.attach(equalizer)
-//        let bands = equalizer.bands
-//        //let globalGain = equalizer.globalGain
-//        let freqs = [60, 230, 910, 4000, 14000]
-//        audioEngine.connect(audioPlayerNode, to: equalizer, format: nil)
-//        audioEngine.connect(equalizer, to: audioEngine.outputNode, format: nil)
-//        equalizer.globalGain = 24
-//        do {
-//                audioFile = try AVAudioFile(forReading: audioURL!)
-//                audioEngine.prepare()
-//                try audioEngine.start()
-//                audioPlayerNode.scheduleFile(audioFile, at: nil, completionHandler: nil)
-//                audioPlayerNode.play()
-//        } catch {
-//            print ("An error occured.")
-//        }
         do
         {
-            print("inside do")
             let audioFile = try AVAudioFile(forReading: audioURL!)
             let readBuffer = AVAudioPCMBuffer.init(pcmFormat: audioFile.processingFormat, frameCapacity: AVAudioFrameCount(audioFile.length))!
             try audioFile.read(into: readBuffer)
-//            print(self.getDecibles1(buffer: readBuffer!))
             let normalizedBuffer = readBuffer.normalize()!
-            
-            var mainMixer = audioEngine.mainMixerNode
             audioEngine.attach(audioFilePlayer)
-            audioEngine.connect(audioFilePlayer, to: mainMixer, format: normalizedBuffer.format)
-            audioEngine.inputNode.outputFormat(forBus: 0)
+            audioEngine.connect(audioFilePlayer, to: audioEngine.outputNode, format: normalizedBuffer.format)
+            
+            //audioEngine.inputNode.outputFormat(forBus: 0)
+            
+            //MARK: - Uncomment to play normalized audio
+            /*
+            try audioEngine.start()
             audioFilePlayer.play()
             audioFilePlayer.scheduleBuffer(normalizedBuffer, completionHandler: nil)
+             */
+            
+            request.append(normalizedBuffer)
+            //request.endAudio()
+            request.shouldReportPartialResults = true
+
+            if (speechRecognizer.isAvailable)
+            {
+                speechRecognizer.recognitionTask(with: request)
+                { result, error in
+                    guard error == nil else { print("Error: \(error!)"); return }
+                    guard let result = result else { print("No result!"); return }
+                    print(result.bestTranscription.formattedString)
+                }
+            }
+            else
+            {
+                print("Device doesn't support speech recognition")
+            }
+            print("done")
         }
         catch let error
         {
             print(error.localizedDescription)
         }
-        let recognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko-KR"))
-        let request = SFSpeechURLRecognitionRequest(url: audioURL!)
-//
-//        request.shouldReportPartialResults = true
-//
-//        if (recognizer?.isAvailable)! {
-//
-//            recognizer?.recognitionTask(with: request) { result, error in
-//                guard error == nil else { print("Error: \(error!)"); return }
-//                guard let result = result else { print("No result!"); return }
-//
-//                print(result.bestTranscription.formattedString)
-//            }
-//        } else {
-//            print("Device doesn't support speech recognition")
-//        }
-//        if let file = try? AVAudioFile(forReading: audioURL!) {
-//            // Set the new max level (in dB) for the gain here.
-//            if let normalizedFile = try? file.normalized(newMaxLevel: -4) {
-//                print(normalizedFile.maxLevel)
-//                // Play your normalizedFile...
-//            }
-//        }
        // startSpeechRecognition()
-    }
-    func getDecibles1(buffer: AVAudioPCMBuffer) -> Float
-    {
-       // This method will compute the Real Mean Squared (RMS) value of an audio
-       // PCM buffer that will return the Decibile (dB) level of an audio signal.
-       // RMS will be calculated for all channels with a signal and averaged
-       // which then provides true dB of the audio for all channels.
-       let frameLength = UInt(buffer.frameLength)
-       let channels = Int(buffer.format.channelCount)
-       var channelsWithSignal = Float(channels)
-       var rms:Float = 0
-       var rmsSumOfChannels: Float = 0
-       for channel in 0..<channels {
-           guard let channelData = buffer.floatChannelData?[channel] else { return 0 }
-           vDSP_measqv(channelData, 1, &rms, frameLength)
-           if rms > 0 {
-               rmsSumOfChannels += rms
-           } else {
-               channelsWithSignal -= 1
-           }
-        }
-        // If you need the average power, uncomment the line below
-        //let avgPower = 20 * log10(rmsSumOfChannels/channelsWithSignal)
-        let dB = 10 * log10(rmsSumOfChannels/channelsWithSignal)
-        return dB
     }
     
     override func viewWillAppear(_ animated: Bool)

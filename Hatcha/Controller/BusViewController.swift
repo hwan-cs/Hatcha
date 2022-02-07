99b7aecda0470a47444dece7fb0d50f9c3376b46
hwan-cs <hwann1005@gmail.com>
2022-02-07 23:55:00 +0900

    Bug Fix, change bundle display name

--- Hatcha/Controller/BusViewController.swift
+++ Hatcha/Controller/BusViewController.swift
@@ -186,6 +186,11 @@ class BusViewController: UIViewController, UISearchBarDelegate
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func selectLineButtonAction(_ sender: UIButton)
+    {
+        lineDropDown.show()
+    }
+    
     @objc func onSaveTap(_ sender: UIBarButtonItem)
     {
 //        //save alarm info in Realm database
@@ -310,37 +315,12 @@ class BusViewController: UIViewController, UISearchBarDelegate
         dropDown.show()
     }
 }
-extension StringProtocol
+extension String
 {
     func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index?
     {
         range(of: string, options: options)?.lowerBound
     }
-    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index?
-    {
-        range(of: string, options: options)?.upperBound
-    }
-    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index]
-    {
-        ranges(of: string, options: options).map(\.lowerBound)
-    }
-    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>]
-    {
-        var result: [Range<Index>] = []
-        var startIndex = self.startIndex
-        while startIndex < endIndex,
-            let range = self[startIndex...]
-                .range(of: string, options: options)
-        {
-                result.append(range)
-                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
-                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
-        }
-        return result
-    }
-}
-extension String
-{
     func index(from: Int) -> Index
     {
         return self.index(startIndex, offsetBy: from)
